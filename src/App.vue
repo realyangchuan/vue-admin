5964a7ebe96791fe08a3d39d425ce8f76b84f9f7
Yang Chuan <realyangchuan@qq.com>
2024-09-25 15:05:04 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+:root {
+  font-size: 62.5%;
+}
+
+body {
+  font-size: 1.6rem;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
